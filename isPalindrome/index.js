bf553a9b2ee38f35a423a58aed65a76bfa7b9b98
Mukul Sharma <mukulsharma262002@gmail.com>
2025-12-15 16:06:36 +0530

    Add isPalindrome function to check if a string is a palindrome

---
+++ isPalindrome/index.js
@@ -0,0 +1,12 @@
+//Write a function to check whether a string is palindrome
+
+const isPalindrome = (str) => {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return str === reversed;
+};
+
+console.log(isPalindrome("wow"));
