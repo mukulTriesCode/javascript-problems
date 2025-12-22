8749cfb8ae387ad6112e3f69f443c79e25707ab0
Mukul Sharma <mukulsharma262002@gmail.com>
2025-12-22 12:48:47 +0530

    Add countVowels function to counts the vowels present in an string

---
+++ countVowels/index.js
@@ -0,0 +1,24 @@
+//Write a function that counts the vowels present
+
+const countVowels = (str) => {
+  const obj = {};
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i].toLowerCase();
+    for (let j = 0; j < vowels.length; j++) {
+      const vowel = vowels[j];
+      if (char === vowel) {
+        if (obj[char]) {
+          obj[char] += 1;
+        } else {
+          obj[char] = 1;
+        }
+      }
+    }
+  }
+
+  return obj;
+};
+
+console.log(countVowels("hello"));
